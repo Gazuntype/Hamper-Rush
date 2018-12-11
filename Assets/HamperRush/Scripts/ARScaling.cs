ab99fef9dd686d04b5d90c1b775ce2888670d8d7
Olutade Ajiboye <mrbigheaddd@gmail.com>
2018-12-11 18:49:15 +0100

    feat: edited HelloAR scene

---
+++ Assets/HamperRush/Scripts/ARScaling.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARScaling : MonoBehaviour {
+	public float scale;
+	public Transform ARCamera;
+
+	// Use this for initialization
+	void Start () {
+		MoveCameraAway();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void MoveCameraAway()
+	{
+		transform.localScale = transform.localScale * scale;
+	}
+}
